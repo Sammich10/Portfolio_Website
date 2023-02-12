0f090a4368f6240d7773f245cf1c04027569c42c
Sammich10 <sammymichelsen@gmail.com>
2023-02-12 00:16:55 -0600

    changed the design of entire top section

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 
 const check = (window.innerHeight - 25); //Point at which fade animation ends (i.e. background will be fulled faded)
+const header = "Sam Michelsen's Website";
+const sub_header = "Software Engineer wannabe; Lover of cats"
+const typedText = document.getElementById("typed-text");
+const typedText_2 = document.getElementById("typed-text2")
+const cursor = document.querySelector('#cursor');
+
+let i = 0;
+setTimeout(function() {
+  (function type() {
+    if (i < header.length) {
+      typedText.innerHTML += header.charAt(i);
+      i++;
+      setTimeout(type, 70);
+    }
+  }());
+},750);
+
+let j = 0;
+setTimeout(function() {
+  (function type2() {
+    if (j < sub_header.length) {
+      typedText_2.innerHTML += sub_header.charAt(j);
+      j++;
+      setTimeout(type2, 70);
+    }
+  }());
+},4000);
 
 window.addEventListener("scroll", () => {
     const currentscroll = window.pageYOffset;
